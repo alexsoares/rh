7c9a4ff178e48de5186e7d47836db659068f8b0a
Manutencao <alexandrejr_soares@yahoo.com.br>
2012-10-24 14:27:58 -0200

    all

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,4 +2,10 @@ class HomesController < ApplicationController
   def index
   end
 
+  def buscar
+    if params[:search].present?
+      @inscricao = Formulario.find(params[:search])
+    end
+  end
+
 end
